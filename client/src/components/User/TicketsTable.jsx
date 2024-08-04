6a5fc6e59ffed7b8aacbe2ac1d6a589c331de67b
me <lopezbeto305@gmail.com>
2024-08-03 23:05:16 -0400

    Updated TicketsTable and TicketsPage to display user tickets with correct aesthetic

--- client/src/components/User/TicketsTable.jsx
+++ client/src/components/User/TicketsTable.jsx
@@ -1,71 +1,76 @@
-import { Box, Typography, useTheme } from "@mui/material";
+import React from 'react';
+import { Box, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { tokens } from "../theme";
-import { mockDataTickets } from "../mockData";
 
-const Team = () => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  const columns = [
-    { field: "id", headerName: "Ticket ID" },
-    { field: "userID", headerName: "User ID" },
-    {
-      field: "type",
-      headerName: "Type",
-    },
-    {
-      field: "subject",
-      headerName: "Subject",
-      flex: 1,
-      //cellClassName: "name-column--cell",
-    },
-    {
-      field: "description",
-      headerName: "Description",
-      flex: 1,
-    },
-    {
-      field: "status",
-      headerName: "Status",
-    },
+const TicketsTable = ({ tickets, onRowClick }) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const columns = [
+        { field: "_id", headerName: "Ticket ID" },
+        { field: "userId", headerName: "User ID" },
+        {
+            field: "topic",
+            headerName: "Type",
+        },
+        {
+            field: "subject",
+            headerName: "Subject",
+            flex: 1,
+            //cellClassName: "name-column--cell",
+        },
+        {
+            field: "description",
+            headerName: "Description",
+            flex: 1,
+        },
+        {
+            field: "status",
+            headerName: "Status",
+        },
+    ];
 
-  ];
-
-  return (
-    <Box m="20px">
-      <Box
-        m="40px 0 0 0"
-        height="75vh"
-        sx={{
-          "& .MuiDataGrid-root": {
-            border: "none",
-          },
-          "& .MuiDataGrid-cell": {
-            borderBottom: "none",
-          },
-          "& .name-column--cell": {
-            color: colors.greenAccent[300],
-          },
-          "& .MuiDataGrid-columnHeaders": {
-            backgroundColor: colors.blueAccent[700],
-            borderBottom: "none",
-          },
-          "& .MuiDataGrid-virtualScroller": {
-            backgroundColor: colors.primary[400],
-          },
-          "& .MuiDataGrid-footerContainer": {
-            borderTop: "none",
-            backgroundColor: colors.blueAccent[700],
-          },
-          "& .MuiCheckbox-root": {
-            color: `${colors.greenAccent[200]} !important`,
-          },
-        }}
-      >
-        <DataGrid checkboxSelection rows={mockDataTickets} columns={columns} />
-      </Box>
-    </Box>
-  );
+    return (
+        <Box m="20px">
+            <Box
+                m="40px 0 0 0"
+                height="75vh"
+                sx={{
+                    "& .MuiDataGrid-root": {
+                        border: "none",
+                    },
+                    "& .MuiDataGrid-cell": {
+                        borderBottom: "none",
+                    },
+                    "& .name-column--cell": {
+                        color: colors.greenAccent[300],
+                    },
+                    "& .MuiDataGrid-columnHeaders": {
+                        backgroundColor: colors.blueAccent[700],
+                        borderBottom: "none",
+                    },
+                    "& .MuiDataGrid-virtualScroller": {
+                        backgroundColor: colors.primary[400],
+                    },
+                    "& .MuiDataGrid-footerContainer": {
+                        borderTop: "none",
+                        backgroundColor: colors.blueAccent[700],
+                    },
+                    "& .MuiCheckbox-root": {
+                        color: `${colors.greenAccent[200]} !important`,
+                    },
+                }}
+            >
+                <DataGrid 
+                    checkboxSelection 
+                    rows={tickets} 
+                    columns={columns} 
+                    getRowId={(row) => row._id} 
+                    onRowClick={onRowClick} 
+                />
+            </Box>
+        </Box>
+    );
 };
 
-export default Team;
+export default TicketsTable;
